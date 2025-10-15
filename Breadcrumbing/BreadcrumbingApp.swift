46d66e0e2f31545d8a82fa1c625aad50d1d39fd9
Renoy Chowdhury <renoy.chowdhury@vidiemme.it>
2025-10-15 19:22:31 +0200

    Initial Commit

---
+++ Breadcrumbing/BreadcrumbingApp.swift
@@ -0,0 +1,17 @@
+//
+//  BreadcrumbingApp.swift
+//  Breadcrumbing
+//
+//  Created by Renoy Chowdhury on 15/10/25.
+//
+
+import SwiftUI
+
+@main
+struct BreadcrumbingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
